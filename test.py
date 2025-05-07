37b607c1f8b1492c008823cd58c1f68b6df119e2
M. Diwakar Reddy <reddydiwakar446@gmail.com>
2025-05-07 15:18:31 +0530

    Updated database schema with foreign key relationships

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
+import mysql.connector
+from dotenv import load_dotenv
 import os
-image_name = "20240612181456_IMG_0868.JPG"
-directory = "static/uploads"
-image_path = os.path.join(directory, image_name)
-print(os.path.exists(image_path))
+
+load_dotenv("server.env")
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE ticket_purchases (
+    purchase_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    event_number INT NOT NULL,
+    purchase_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (purchase_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (event_number) REFERENCES events(event_number)
+);
+""")
