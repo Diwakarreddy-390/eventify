9182bdeb976c3e5ed3d4c346a20b1bd7ea0c3cd7
M. Diwakar Reddy <reddydiwakar446@gmail.com>
2025-05-10 14:05:06 +0530

    Updated data with latest changes

--- test.py
+++ test.py
@@ -1,39 +1,10 @@
-import mysql.connector
-from dotenv import load_dotenv
-import os
-import base64
-from PIL import Image
-from io import BytesIO
-
-# Load environment variables
-load_dotenv("sql.env")
-
-# Function to establish connection
-def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+from db_connecetion import get_connection
 
 # Connect to database
 conn = get_connection()
 cursor = conn.cursor()
 
-# Fetch image data
-email = "raju@gmail.com"
-cursor.execute("SELECT photo FROM events WHERE created=%s", (email,))
-img = cursor.fetchone()
-
-if img:
-    image_bytes = img[0]  # Extract binary image data
-    image = Image.open(BytesIO(image_bytes))
-
-    # Show image
-    image.show()
-
-
-# Close connections
+cursor.execute("""DROP TABLE events;
+""")
 cursor.close()
 conn.close()
