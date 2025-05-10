449dc455a4da678c0004cf0b4c183629f70b6c02
M. Diwakar Reddy <reddydiwakar446@gmail.com>
2025-05-10 14:18:59 +0530

    Updated data with latest changes

--- test.py
+++ test.py
@@ -3,8 +3,15 @@ from db_connecetion import get_connection
 # Connect to database
 conn = get_connection()
 cursor = conn.cursor()
-
-cursor.execute("""DROP TABLE events;
+email = "mouni@gmail.com"
+cursor.execute("""CREATE TABLE ticket_purchases (
+    purchase_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    event_number INT,
+    purchase_time TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (event_number) REFERENCES events(event_number)
+);
 """)
-cursor.close()
-conn.close()
+
+
